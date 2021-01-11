e7a54036b6be75ef2b0346e5c062afd9cffe4468
chrisolorobin <camorton88@gmail.com>
2021-01-11 13:03:51 -0800

    completed cat seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+cats = [
+    {    
+        name:"Cody",
+        age: 4,
+        enjoys:"playing with strings"
+     },
+     {  
+        name:"Cindy",
+        age: 5,
+        enjoys:"hanging out in the barn"
+     },
+     {   
+        name:"Jack",
+        age: 3,
+        enjoys:"spending time with the cows"
+     },
+  ]
+  
+  cats.each do |attributes|
+    Cat.create attributes
+    puts "creating cat #{attributes}"
+  end
